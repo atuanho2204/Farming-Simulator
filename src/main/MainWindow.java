e11e2a28be68489d2611e3e077e180da341769f9
Quynh Trinh <qtrinh6@gatech.edu>
2020-09-20 15:23:06 -0400

    add text to welcome screen in MainWindow

--- src/main/MainWindow.java
+++ src/main/MainWindow.java
@@ -1,28 +1,19 @@
 package main;
 
 import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-
-import java.util.ArrayList;
 
 public class MainWindow extends Application {
     /**
      * main method to help launch the program
      * @param args unused parameter
+     * @throws java.lang.Exception throws exception
      */
     public static void main(String[] args) throws Exception {
         launch(args);
@@ -36,21 +27,31 @@ public class MainWindow extends Application {
     public void start(Stage primaryStage) {
         VBox vBox = new VBox();
 
-        VBox vboxForText = new VBox();
-        vboxForText.setMinHeight(500);
+        VBox vBoxForText = new VBox();
+        vBoxForText.setMinHeight(500);
+        Text welcomeFirstLine = new Text("Welcome to the");
+        Text welcomeSecondLine = new Text("Totally Accurate Farming Simulation!");
+        welcomeFirstLine.setStyle("-fx-font: 75 arial;");
+        welcomeSecondLine.setStyle("-fx-font: 75 arial;");
+        vBoxForText.setAlignment(Pos.CENTER);
+        vBoxForText.getChildren().addAll(welcomeFirstLine, welcomeSecondLine);
 
-        HBox hboxForButtons = new HBox();
+        HBox hBoxForButtons = new HBox();
         Button startButton = new Button("Start");
-        startButton.setStyle("-fx-background-color: green; -fx-text-fill: white; -fx-font: 50 arial;"
-                + "-fx-border-radius: 20; -fx-background-radius: 20");
+        startButton.setStyle("-fx-background-color: green; -fx-text-fill: white;"
+                + "-fx-font: 50 arial; -fx-border-radius: 20; -fx-background-radius: 20");
         Button quitButton = new Button("Quit");
-        quitButton.setStyle("-fx-background-color: green; -fx-text-fill: white; -fx-font: 50 arial;"
-                + "-fx-border-radius: 20; -fx-background-radius: 20");
-        hboxForButtons.getChildren().addAll(startButton, quitButton);
-        hboxForButtons.setSpacing(100);
-        hboxForButtons.setAlignment(Pos.CENTER);
+        quitButton.setStyle("-fx-background-color: green; -fx-text-fill: white;"
+                + "-fx-font: 50 arial; -fx-border-radius: 20; -fx-background-radius: 20");
+        /*Button resumeButton = new Button("Resume");
+        resumeButton.setStyle("-fx-background-color: green; -fx-text-fill: white;
+                + "-fx-font: 50 arial; -fx-border-radius: 20; -fx-background-radius: 20");
+        hBoxForButtons.getChildren().addAll(startButton, resumeButton, quitButton);*/
+        hBoxForButtons.getChildren().addAll(startButton, quitButton);
+        hBoxForButtons.setSpacing(100);
+        hBoxForButtons.setAlignment(Pos.CENTER);
 
-        vBox.getChildren().addAll(vboxForText, hboxForButtons);
+        vBox.getChildren().addAll(vBoxForText, hBoxForButtons);
 
         primaryStage.setTitle("Totally Accurate Farming Simulation");
         primaryStage.setScene(new Scene(vBox));
