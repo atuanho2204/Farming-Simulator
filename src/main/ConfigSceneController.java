55a7305cf13774b24a4703b73267565baf6d3e01
Quynh Trinh <qtrinh6@gatech.edu>
2020-09-23 15:10:54 -0400

    update ConfigSceneController.java to change positions of items

--- src/main/ConfigSceneController.java
+++ src/main/ConfigSceneController.java
@@ -1,14 +1,23 @@
 package main;
 
-//import javafx.event.ActionEvent;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
-//import javafx.stage.Window;
+import javafx.stage.Stage;
+
 
 public class ConfigSceneController {
+    private int sceneWidth = 1200;
+    private int sceneHeight = 800;
+
+    @FXML
+    private TextField playerName;
     @FXML
     private ToggleGroup seasonGroup;
-
     @FXML
     private CheckBox wheat;
     @FXML
@@ -19,14 +28,18 @@ public class ConfigSceneController {
     private CheckBox lettuce;
 
     @FXML
-    public void getSeason() throws Exception {
+    private void initialize() {
+    }
+
+    @FXML
+    public void getSeason() {
         RadioButton selectedRadioButton = (RadioButton) seasonGroup.getSelectedToggle();
         String value = selectedRadioButton.getText();
         System.out.println(value);
     }
 
     @FXML
-    public void getSeed() throws Exception {
+    public void getSeed() {
         if (wheat.isSelected()) {
             System.out.println(wheat.getText());
         }
@@ -40,9 +53,28 @@ public class ConfigSceneController {
             System.out.println(lettuce.getText());
         }
     }
-    @FXML
-    private void initialize() {
+
+    public void handleContinueAction(ActionEvent event) {
+        Stage primaryStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        try {
+            Parent gamePane = FXMLLoader.load(getClass().getResource("gameScene.fxml"));
+            if (!playerName.getText().isEmpty()) {
+                Scene gameScene = new Scene(gamePane, sceneWidth, sceneHeight);
+                primaryStage.setScene(gameScene);
+                primaryStage.show();
+                Scene scene = new Scene(gamePane);
+                primaryStage.setScene(scene);
+                primaryStage.show();
+            } else {
+                throw new Exception();
+            }
+        } catch (Exception e) {
+            System.out.println("Name is required.");
+        }
     }
 
-
+    public void handleQuitAction(ActionEvent event) {
+        Stage primaryStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        primaryStage.close();
+    }
 }
