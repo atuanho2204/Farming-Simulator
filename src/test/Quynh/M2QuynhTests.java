8f8d7ed315d24eda8b184d2d301ad5339b978ddd
Quynh Trinh <qtrinh6@gatech.edu>
2020-09-26 17:33:05 -0400

    add first test

--- src/test/Quynh/M2QuynhTests.java
+++ src/test/Quynh/M2QuynhTests.java
@@ -1,5 +1,20 @@
 package test.Quynh;
 
+import main.configurationScreen.ConfigSceneController;
+import org.junit.Before;
+import org.junit.Test;
+
 public class M2QuynhTests {
-    // placeholder for my folder
+    private ConfigSceneController configScene;
+
+    @Before
+    public void setUp() {
+        configScene = new ConfigSceneController();
+    }
+
+    @Test (expected = ExceptionInInitializerError.class)
+    public void testPath() {
+        String path = "farm/farmUI.fxml";
+        configScene.loadNextScene(path);
+    }
 }
