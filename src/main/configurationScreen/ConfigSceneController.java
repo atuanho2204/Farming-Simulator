399ae648cfcf724945b536f6fbdcd1a3a1fbf560
Reid, Christopher Edward <creid61@gatech.edu>
2020-09-27 10:27:57 -0400

    Adding and Deleting getters
    I am creating a test class for my getters to initialize from my farmController class

--- src/main/configurationScreen/ConfigSceneController.java
+++ src/main/configurationScreen/ConfigSceneController.java
@@ -3,7 +3,6 @@ package main.configurationScreen;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
