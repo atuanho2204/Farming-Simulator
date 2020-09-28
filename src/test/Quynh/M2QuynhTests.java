39d0c0697300d56bd3fe10541bedfd26ecf46438
Quynh Trinh <qtrinh6@gatech.edu>
2020-09-27 23:41:09 -0400

    fix checkstyle

--- src/test/Quynh/M2QuynhTests.java
+++ src/test/Quynh/M2QuynhTests.java
@@ -18,8 +18,9 @@ public class M2QuynhTests {
     /**
      *
      * Test Component: player’s name
-     * Reason: We need to check that the name entered by the player has at least one character.
-     *    In other words, the player’s name is not allowed to be an empty String.
+     * Reason: We need to check that the name entered by the player has at least
+     *    one character. In other words, the player’s name is not allowed to be
+     *    an empty String.
      * Method: Create a constructor in which the parameter for name is an empty String.
      *    The unit test expects an exception.  In the game, when this exception occurs,
      *    an error will pop up with the message “Your name must have at least 1 character.”
@@ -34,16 +35,19 @@ public class M2QuynhTests {
     /**
      *
      * Test Component: one of the ToggleGroup object--season
-     * Reason: We need to make sure that the player chooses a starting season in order to begin the game.
+     * Reason: We need to make sure that the player chooses a starting season in order
+     *    to begin the game.
      * Method: Create a constructor in which the parameter for season is null.
-     *    The unit test expects an exception.  In the game, when this exception occurs, an error will
-     *    pop up with the message “Please enter your name and select difficulty, season, seed before start!!!”
+     *    The unit test expects an exception.  In the game, when this exception occurs,
+     *    an error will pop up with the message “Please enter your name and select difficulty,
+     *    season, seed before start!!!”
      *
      */
     @Test (expected = ExceptionInInitializerError.class)
     public void testNameOfEmptyString() {
         List<String> seeds =  new ArrayList<>();
         seeds.add("cotton");
-        ConfigSceneController configScene1 = new ConfigSceneController(1, "", seeds, "fall");
+        ConfigSceneController configScene1 = new ConfigSceneController(
+                1, "", seeds, "fall");
     }
 }
