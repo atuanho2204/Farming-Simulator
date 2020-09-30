0ba76a8317e3a3dbc5b328a233ec1aab6f603750
Quynh Trinh <qtrinh6@gatech.edu>
2020-09-29 23:33:20 -0400

    handle removing dead crops

--- src/main/farm/FarmController.java
+++ src/main/farm/FarmController.java
@@ -1,14 +1,13 @@
 package main.farm;
 
-import com.sun.javafx.geom.Vec2d;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import main.gameManager.GameManager;
 import main.gameManager.NewDayListener;
@@ -17,9 +16,7 @@ import main.util.Crop;
 import main.util.CropStage;
 import main.util.CropTypes;
 
-import java.net.URL;
 import java.util.ArrayList;
-import java.util.ResourceBundle;
 
 
 /**
@@ -109,20 +106,26 @@ public class FarmController implements NewDayListener {
 
     private void populatePlotsRandomly() {
         //replace this
+        initializeUIPlots();
         int numOfSeedTypes = gameManager.getSeeds().size();
+        int numOfStages = CropStage.values().length;
         for (int i = 0; i < NUM_OF_PLOTS; ++i) {
-            int randomCrop = (int) (Math.random() * 10) % numOfSeedTypes;
-            int randomStage = (int) (Math.random() * 10) % CropStage.values().length;
+            int randomCrop = (int) (Math.random() * 100) % numOfSeedTypes;
+            int randomStage = (int) (Math.random() * 100) % numOfStages;
             String seed = gameManager.getSeeds().get(randomCrop).toUpperCase();
-            plots.add(new Plot(new Crop(CropTypes.valueOf(seed), CropStage.values()[randomStage])));
-
+            plots.add(new Plot(new Crop(CropTypes.valueOf(seed), CropStage.values()[randomStage]), uiPlots.get(i)));
             //plots.add(new Plot(new Vec2d(0,0), new Crop(CropTypes.valueOf(seed))));
         }
-        showPlots();
+        for (int i = 0; i < NUM_OF_PLOTS; ++i) {
+            //uiPlots.get(i).setPrefSize(20,20);
+            //uiPlots.get(i).setGraphic(new ImageView(new Image("main/images/Untitled_Artwork.jpg")));
+            uiPlots.get(i).setText(plots.get(i).getCurrentCrop().getType().toString()
+                    + "\n" + plots.get(i).getCurrentCrop().getStage().toString());
+        }
     }
 
     @FXML
-    private void showPlots() {
+    private void initializeUIPlots() {
         uiPlots.add(plot00);
         uiPlots.add(plot01);
         uiPlots.add(plot02);
@@ -135,17 +138,28 @@ public class FarmController implements NewDayListener {
         uiPlots.add(plot21);
         uiPlots.add(plot22);
         uiPlots.add(plot23);
-        for (int i = 0; i < NUM_OF_PLOTS; ++i) {
-            //uiPlots.get(i).setPrefSize(20,20);
-            //uiPlots.get(i).setGraphic(new ImageView(new Image("main/images/Untitled_Artwork.jpg")));
-            uiPlots.get(i).setText(plots.get(i).getCurrentCrop().getType().toString()
-                    + "\n" + plots.get(i).getCurrentCrop().getStage().toString());
-        }
     }
 
     @FXML
-    public void harvestPlot(ActionEvent event) {
-        //make code to check the plots instance variable
+    public void harvestPlot() {
+        for (Plot plot : plots) {
+            plot.getPlotButton().setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent actionEvent) {
+                    if (plot.getCurrentCrop().getStage() == CropStage.DEAD) {
+                        plot.getPlotButton().setText("");
+                    }
+                    if (plot.getCurrentCrop().getStage() == CropStage.MATURE) {
+                        try {
+                            getAlert("Storage is full!!!"); // for testing only
+                            // try to store harvest
+                        } catch (Exception e) {
+                            getAlert("Storage is full!!!");
+                        }
+                    }
+                }
+            });
+        }
     }
 
     @FXML
@@ -157,5 +171,13 @@ public class FarmController implements NewDayListener {
     public void handleQuitButton(ActionEvent event) {
         primaryStage.close();
         gameManager.getTimeAdvancer().pauseTime();
+    }
+
+    public void getAlert(String message) {
+        Alert a = new Alert(Alert.AlertType.NONE);
+        a.setAlertType(Alert.AlertType.INFORMATION);
+        a.setContentText(message);
+        // show the dialog
+        a.show();
     }
 }
