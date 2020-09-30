efdf5c3ca026815f51f48045fc37ef2b53843795
Quynh Trinh <qtrinh6@gatech.edu>
2020-09-29 23:33:46 -0400

    update constructor to take in Button as a parameter

--- src/main/farm/Plot.java
+++ src/main/farm/Plot.java
@@ -32,27 +32,38 @@ public class Plot {
 
 package main.farm;
 
-import com.sun.javafx.geom.Vec2d;
 import main.util.Crop;
 import main.util.CropStage;
 import main.util.CropTypes;
 
+import javafx.scene.control.Button;
+
 public class Plot {
     private Crop currentCrop;
+    private Button plotButton;
 
     public Plot() {
-        this(new Crop(CropTypes.CORN, CropStage.SPROUT));
+        this(new Crop(CropTypes.CORN, CropStage.SPROUTING), new Button());
     }
 
-    public Plot(Crop currentCrop) {
+    public Plot(Crop currentCrop, Button plotButton) {
         this.currentCrop = currentCrop;
+        this.plotButton = plotButton;
     }
 
     public void setCurrentCrop(Crop currentCrop) {
         this.currentCrop = currentCrop;
     }
 
+    public void setPlotButton(Button plotButton) {
+        this.plotButton = plotButton;
+    }
+
     public Crop getCurrentCrop() {
         return currentCrop;
+    }
+
+    public Button getPlotButton() {
+        return plotButton;
     }
 }
