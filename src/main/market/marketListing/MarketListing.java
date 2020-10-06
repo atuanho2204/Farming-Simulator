8e51b80ea0d247fcde87b9cf7ead2958af84c21e
Reid, Christopher Edward <creid61@gatech.edu>
2020-10-05 21:18:09 -0400

    Fixing UI
    Fixing the OnAction for the Inventory UI

--- src/main/market/marketListing/MarketListing.java
+++ src/main/market/marketListing/MarketListing.java
@@ -1,7 +1,6 @@
 package main.market.marketListing;
 
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.text.Text;
