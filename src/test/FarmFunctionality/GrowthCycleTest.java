7e9056eb4dccece92b3abc26d7bd6785d2053226
aho71 <aho71@gatech.edu>
2020-10-24 13:17:36 -0400

    rename growthcycle test file

--- src/test/FarmFunctionality/GrowthCycleTest.java
+++ src/test/FarmFunctionality/GrowthCycleTest.java
@@ -16,7 +16,7 @@ import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
-public class LifeCycle {
+public class GrowthCycleTest {
     private Plot plot;
     private FarmController controller;
     private InventoryUIController inventoryUI;
