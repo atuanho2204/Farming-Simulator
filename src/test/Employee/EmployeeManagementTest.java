80ffebd9d9ded857f9d6f46c95e6dee735a0320e
aho71 <aho71@gatech.edu>
2020-11-06 03:28:42 -0500

    add employee tests

--- src/test/Employee/EmployeeManagementTest.java
+++ src/test/Employee/EmployeeManagementTest.java
@@ -1,9 +1,16 @@
 package test.Employee;
 
 import main.employment.EmployeeManager;
+import main.farm.FarmState;
+import main.farm.crops.CropStages;
+import main.farm.crops.CropTypes;
+import main.farm.plot.Plot;
 import main.gameManager.GameManager;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
@@ -12,6 +19,7 @@ import static org.junit.Assert.assertEquals;
  */
 public class EmployeeManagementTest {
     private EmployeeManager employeeManager;
+    private List<Plot> plots = new ArrayList<>(4);
 
     @Before
     public void setup() {
@@ -66,8 +74,56 @@ public class EmployeeManagementTest {
 
     // @Tuan
     // test adding too many employees?
+    @Test
+    public void testHireTooManyEmployee() {
+        employeeManager.addHarvester(GameManager.getInstance().getDay());
+        employeeManager.addHarvester(GameManager.getInstance().getDay());
+        employeeManager.addHarvester(GameManager.getInstance().getDay());
+        employeeManager.addHarvester(GameManager.getInstance().getDay());
+        employeeManager.addHarvester(GameManager.getInstance().getDay());
+        employeeManager.addHarvester(GameManager.getInstance().getDay());
+        employeeManager.addHarvester(GameManager.getInstance().getDay());
+        employeeManager.addHarvester(GameManager.getInstance().getDay());
+        assertEquals(6, employeeManager.getEmployees().size());
+    }
+
     // test the payEmployees
+    @Test
+    public void testPayEmployee() {
+        int dailyWage = employeeManager.getTotalSalary();
+        employeeManager.payWages();
+        int currentMoney = GameManager.getInstance().getMoney();
+        assertEquals(10000 - dailyWage, currentMoney);
+    }
+
     // test the harvesting/selling
+    @Test
+    public void testHarvesting() {
+        employeeManager.addHarvester(GameManager.getInstance().getDay());
+
+    }
+
+
     // test the employees leaving because of no money
+    @Test
+    public void testLeavingWithoutMoney() {
+        employeeManager.addHarvester(GameManager.getInstance().getDay());
+        employeeManager.addHarvester(GameManager.getInstance().getDay());
+        employeeManager.addHarvester(GameManager.getInstance().getDay());
+        employeeManager.addHarvester(GameManager.getInstance().getDay());
+        employeeManager.addHarvester(GameManager.getInstance().getDay());
+        employeeManager.addHarvester(GameManager.getInstance().getDay());
+        assertEquals(6, employeeManager.getEmployees().size());
+        GameManager.getInstance().setMoney(0);
+        employeeManager.payWages();
+        employeeManager.payWages();
+        employeeManager.payWages();
+        assertEquals(3, employeeManager.getEmployees().size());
+    }
     // try deleting a harvestor/seller when there are none
+    @Test
+    public void testNoneEmployee() {
+        employeeManager.deleteHarvester();
+        employeeManager.deleteSeller();
+    }
 }
