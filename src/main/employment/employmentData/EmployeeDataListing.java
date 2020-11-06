51efd996fb67d947653f65c934c8f69dd4f0f4bd
aho71 <aho71@gatech.edu>
2020-11-06 02:25:45 -0500

    modifie employeeUI skill level display

--- src/main/employment/employmentData/EmployeeDataListing.java
+++ src/main/employment/employmentData/EmployeeDataListing.java
@@ -1,14 +1,20 @@
 package main.employment.employmentData;
 
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import main.employment.Employee;
 import main.employment.EmployeeManager;
 import main.employment.EmployeeTypes;
+import main.gameManager.GameManager;
+
+import javax.swing.*;
 
 
 public class EmployeeDataListing {
-    public static Text getListingUI(Employee employee) {
+    public static HBox getListingUI(Employee employee) {
+        HBox hbox = new HBox(5);
         Text text = new Text();
         EmployeeTypes type = employee.getEmployeeType();
         String t = "";
@@ -18,12 +24,23 @@ public class EmployeeDataListing {
         if (type == EmployeeTypes.SELLER) {
             t = "Seller";
         }
-        text.setText(employee.getEmployeeName() +  ": "
+        int skill = employee.getSkillLevel();
+        text.setText(employee.getEmployeeName() +  " (" + "LV " + skill + "): "
                 + t + " $" + EmployeeManager.getSalaryFromSkillLevel(employee.getSkillLevel()));
 
-        text.setStyle("-fx-font: 16 chalkduster;"
+        text.setStyle("-fx-font: 14 chalkduster;"
                 + "-fx-padding-left: 15;");
         text.setFill(Color.WHITE);
-        return text;
+
+        Button fire = new Button();
+        fire.setText("Fire");
+        fire.setStyle("-fx-margin: 0 10 0 0;");
+        fire.setTextFill(Color.DARKRED);
+
+        fire.setOnAction(e -> {
+            GameManager.getInstance().getEmployees().fireEmployee(employee);
+        });
+        hbox.getChildren().addAll(fire, text);
+        return hbox;
     }
 }
