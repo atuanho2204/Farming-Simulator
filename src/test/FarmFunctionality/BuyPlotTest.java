be286bf5912b5b0812a9e66d5a5ec7ac4c8d8da2
dnguyen379 <dnguyen379@gatech.edu>
2020-11-22 04:47:45 -0500

    Add test Duy Nguyen

---
+++ src/test/FarmFunctionality/BuyPlotTest.java
@@ -0,0 +1,33 @@
+package test.FarmFunctionality;
+
+import main.farm.FarmState;
+import main.farm.crops.CropStages;
+import main.farm.plot.Plot;
+import main.gameManager.GameManager;
+import main.inventory.Inventory;
+import main.farm.crops.CropTypes;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BuyPlotTest {
+
+    @Before
+    public void setUp() {
+        FarmState.clearFarmStateDangerous();
+        GameManager.getInstance().clear();
+        GameManager.getInstance().setInventory(new Inventory(true));
+    }
+
+    /**
+     * Check the initial count of the farm and keep track of the plot counts
+     * Author: Duy
+     */
+    @Test
+    public void testFertizePlot() {
+        Assert.assertEquals(6, FarmState.getInstance().getPlotCount());
+        FarmState.getInstance().increasePlotCount();
+        FarmState.getInstance().increasePlotCount();
+        Assert.assertEquals(8, FarmState.getInstance().getPlotCount());
+    }
+}
